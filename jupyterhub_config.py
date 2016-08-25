7a2e095ecf9868fbceab018ee68aa8812c5fdf96
Martin Angelov <m.angelov95@gmail.com>
2016-08-25 16:28:09 +0100

    Added environment variables for Swarm TLS
    The values of the environment variables will be set from salt.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -50,9 +50,9 @@ c.SystemUserSpawner.remove_containers = True
 # TLS CONFIG FOR DOCKER SWARM
 
 c.SystemUserSpawner.tls_verify = True
-c.SystemUserSpawner.tls_ca = os.environ['DOCKER_CERT_PATH'] + '/ca.pem'
-c.SystemUserSpawner.tls_cert = os.environ['DOCKER_CERT_PATH'] + '/cert.pem' 
-c.SystemUserSpawner.tls_key = os.environ['DOCKER_CERT_PATH'] + '/key.pem' 
+c.SystemUserSpawner.tls_ca = os.environ['TLS_CA_CERT']
+c.SystemUserSpawner.tls_cert = os.environ['TLS_CERT']
+c.SystemUserSpawner.tls_key = os.environ['TLS_KEY'] 
 
 # END OF TLS CONFIG FOR DOCKER SWARM
 
